6d12008f5e71142a6c468f6e949916132061169f
Quinn Michaels <quinn@indra.ai>
2026-06-29 20:34:06 -0700

    Update Deva Core and Owner Signature Required.

--- feature/methods.js
+++ feature/methods.js
@@ -1,7 +1,7 @@
 "use strict";
 // Data Deva Feature Methods
 // Copyright ©2000-2026 Quinn A Michaels; All rights reserved. 
-// Legal Signature Required For Lawful Use.
+// Owner Signature Required For Lawful Use.
 // Distributed under VLA:62635860369399044964 LICENSE.md
 // Fri, 26 Jun 2026 05:18:59 GMT PST
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 // Data Deva
 // Copyright ©2000-2026 Quinn America Michaels; All rights reserved. 
-// Legal Signature Required For Lawful Use.
+// Owner Signature Required For Lawful Use.
 // Distributed under VLA:62635860369399044964 LICENSE.md
 // Thursday, June 25, 2026 - 10:18:59 PM PST
 
